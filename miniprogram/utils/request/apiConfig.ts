15723c1869841595a288425baf6869125382ef0c
wangxiaobo <2078350663@qq.com>
2024-01-04 15:20:11 +0800

    change api

--- miniprogram/utils/request/apiConfig.ts
+++ miniprogram/utils/request/apiConfig.ts
@@ -1,11 +1,11 @@
 let baseUrlPrefix = "";
 let baseFilePrefix = "";
-// const hostApiDev = "http://172.16.6.20:8082";
+const hostApiDev = "http://172.16.6.20:8082";
 // const hostApiDev = "http://10.211.255.228:8082";
-const hostApiDev = "https://yndrc.yn.gov.cn:8084";
-const hostApiTrial = "https://yndrc.yn.gov.cn:8084";
-const hostApiProd = "https://yndrc.yn.gov.cn:8084";
-const file = "https://yndrc.yn.gov.cn:8084";
+// const hostApiDev = "https://yndrc.yn.gov.cn:8084";
+const hostApiTrial = "http://172.16.6.20:8082";
+const hostApiProd = "http://172.16.6.20:8082";
+const file = "http://172.16.6.20:8082";
 const accountInfo = wx.getAccountInfoSync();
 const env = accountInfo.miniProgram.envVersion;
 
